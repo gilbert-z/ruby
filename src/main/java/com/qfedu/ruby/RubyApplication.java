46389d4be9ef93cabd8dca84c27c687102eddfc8
gilbert-z <274138089@qq.com>
2019-01-07 17:06:53 +0800

    first

---
+++ src/main/java/com/qfedu/ruby/RubyApplication.java
@@ -0,0 +1,14 @@
+package com.qfedu.ruby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubyApplication.class, args);
+    }
+
+}
+
