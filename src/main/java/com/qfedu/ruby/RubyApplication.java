246b606eae18b2565b20dfdc73528587500038b6
qc <897294040@qq.com>
2019-01-08 17:35:45 +0800

    111

--- src/main/java/com/qfedu/ruby/RubyApplication.java
+++ src/main/java/com/qfedu/ruby/RubyApplication.java
@@ -1,8 +1,10 @@
 package com.qfedu.ruby;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value="com.qfedu.ruby.dao")
 @SpringBootApplication
 public class RubyApplication {
 
--- src/main/java/com/qfedu/ruby/controller/UserController.java
+++ src/main/java/com/qfedu/ruby/controller/UserController.java
@@ -1,16 +1,30 @@
 package com.qfedu.ruby.controller;
 
+import com.qfedu.ruby.dao.TshareMapper;
+import com.qfedu.ruby.pojo.Tshare;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Api(value = "用户登录注册模块",tags = "登录，注册")
 @RestController
 public class UserController {
 
+    @Autowired
+    private TshareMapper mapper;
+
     @ApiOperation(value = "打招呼",httpMethod = "GET",notes = "实现了打招呼功能")
     @GetMapping("hi")
     String sayhi(){
         return "hello";
+    }
+
+
+    @GetMapping("hh")
+    void hh(){
+        Tshare t = new Tshare();
+        t.setContent("asdsadsa");
+        mapper.insert(t);
     }
 }
