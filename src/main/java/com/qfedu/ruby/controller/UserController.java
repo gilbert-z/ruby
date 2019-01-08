64be39afb21318619815ac64c4f7cec77a7c73e0
gilbert-z <274138089@qq.com>
2019-01-08 11:12:32 +0800

    vo

--- src/main/java/com/qfedu/ruby/controller/UserController.java
+++ src/main/java/com/qfedu/ruby/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.qfedu.ruby.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(value = "用户登录注册模块",tags = "登录，注册")
 @RestController
 public class UserController {
 
-
+    @ApiOperation(value = "打招呼",httpMethod = "GET",notes = "实现了打招呼功能")
     @GetMapping("hi")
     String sayhi(){
         return "hello";
