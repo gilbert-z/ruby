fcad75cf3cb52f37d74043db9fd362a165dd5295
gilbert-z <274138089@qq.com>
2019-01-11 22:08:10 +0800

    地址接口完成

--- src/main/java/com/qfedu/ruby/dao/TaddressMapper.java
+++ src/main/java/com/qfedu/ruby/dao/TaddressMapper.java
@@ -2,7 +2,11 @@ package com.qfedu.ruby.dao;
 
 import com.qfedu.ruby.pojo.Taddress;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
 @Component(value = "TaddressMapper")
@@ -18,4 +22,12 @@ public interface TaddressMapper {
     int updateByPrimaryKeySelective(Taddress record);
 
     int updateByPrimaryKey(Taddress record);
+
+    List<Taddress> selectAll(Integer uid);
+
+    int updateByFlag(Integer uid);
+
+    int updateFlagByUid(@Param("id") Integer id);
+
+    Taddress selectFlagByUid(Integer uid);
 }
