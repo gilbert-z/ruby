8c680c7a6a273c73b130f828cd666451d210824e
gilbert-z <274138089@qq.com>
2019-01-12 21:01:21 +0800

    根据价格排序

--- src/main/java/com/qfedu/ruby/controller/GoodsController.java
+++ src/main/java/com/qfedu/ruby/controller/GoodsController.java
@@ -4,18 +4,22 @@ import com.qfedu.ruby.pojo.Tgoods;
 import com.qfedu.ruby.service.GoodsService;
 import com.qfedu.ruby.util.ResultBean;
 import com.qfedu.ruby.util.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Api(value = "首页特价等链接",tags = "根据特点查询商品详情")
 @RestController
 public class GoodsController {
 
     @Autowired
     private GoodsService goodsService;
-
+    @ApiImplicitParam(name = "gtype", value = "商品类型", required=true,paramType="query")
+    @ApiOperation(value = "获取商品列表",httpMethod = "GET",notes = "根据传入类型")
     @GetMapping("goods/goodslist.do")
     ResultBean goodsList(Integer gtype){
         if (gtype==null){
@@ -24,7 +28,7 @@ public class GoodsController {
             return goodsService.showGoodslist(gtype);
         }
     }
-
+    @ApiOperation(value = "特价产品",httpMethod = "GET",notes = "展示指定列表")
     @GetMapping("goods/showSale.do")
     ResultBean showSale(){
         ResultBean resultBean = goodsService.showSale();
@@ -32,14 +36,14 @@ public class GoodsController {
         return resultBean;
     }
 
-
+    @ApiOperation(value = "贱货排名",httpMethod = "GET",notes = "展示指定列表")
     @GetMapping("goods/showExpensive.do")
     ResultBean showExpensive(){
         ResultBean resultBean = goodsService.showExpensive();
 
         return resultBean;
     }
-
+    @ApiOperation(value = "新品上架",httpMethod = "GET",notes = "展示指定列表")
     @GetMapping("goods/showNewGoods.do")
     ResultBean showNewGoods(){
         ResultBean resultBean = goodsService.showNewGoods();
